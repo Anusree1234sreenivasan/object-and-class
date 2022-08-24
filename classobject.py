1acd73e778d82876cd49d63b3cbca81be6f69d5b
Anusree1234sreenivasan <110896832+Anusree1234sreenivasan@users.noreply.github.com>
2022-08-24 15:14:48 +0530

    1st commit

---
+++ classobject.py
@@ -0,0 +1,10 @@
+class Anu:
+    attr1="Anu"
+    attr2="21"
+    def qualification(self):
+        print("My Name is",self.attr1)
+        print("My Age is",self.attr2)
+student = Anu()
+print(student.attr1)
+student.qualification()
+
